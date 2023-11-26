b17bdec2d8cc32062daa225aca5b98eebbd32701
Fernando <eze980523@gmail.com>
2023-11-26 01:13:00 -0300

    Desarrollo de la clase AccesoDatos

--- Control de ingresos/AccesoDatos.cs
+++ Control de ingresos/AccesoDatos.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 
 namespace Control_de_ingresos
 {
-    internal class AccesoDatos
+    public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private static string cadena_conexion;
+
+        static AccesoDatos()
+        {
+
+        }
     }
 }
