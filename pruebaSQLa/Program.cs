00d955165362ca99ec0d5e62e36d1ca605f3f84b
Fernando <eze980523@gmail.com>
2023-11-26 01:40:52 -0300

    Desarrollo de una poryeto de consola para testeo

---
+++ pruebaSQLa/Program.cs
@@ -0,0 +1,19 @@
+﻿using Control_de_ingresos;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        AccesoDatos ado = new AccesoDatos();
+
+        if (ado.Prueba())
+        {
+            Console.WriteLine("Se conecto");
+        }
+        else
+        {
+            Console.WriteLine("No se conecto");
+        }
+
+        Console.ReadKey();
+    }
+}
