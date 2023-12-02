0253541a6a7af9e1801294696a23d6160092a941
Fernando <eze980523@gmail.com>
2023-12-02 15:02:44 -0300

    Testeo del metodo EliminarProducto

--- pruebaSQLa/Program.cs
+++ pruebaSQLa/Program.cs
@@ -12,16 +12,11 @@ internal class Program
 
         Gaseosa gaseosa1 = new Gaseosa("Seven up", "Gaseosa", (EMarca)Enum.Parse(typeof(EMarca), "SevenUp"), 3, 500f, 3f, "POMELO");
         GaseosaPorMayor gaseosaPorMayor1 = new GaseosaPorMayor("Trini", "Gaseosa Mayorista", (EMarca)Enum.Parse(typeof(EMarca), "Vienissima"), 4, 300f, 500f, "Uva", 1000, "Si");
-        Milanesas milanga = new Milanesas("Fernando", "Milanesas", (EMarca)Enum.Parse(typeof(EMarca), "Gallo"), 20, 300f, "Vacuno", "Brasil");
+        Milanesas milanga = new Milanesas("Fernando", "Milanesas", (EMarca)Enum.Parse(typeof(EMarca), "Gallo"), 20, 300f, "Vacuno", "Paraguay");
 
 
 
-        List<Producto> list = DatosTabla<Producto>.UnirListas();
-
-        foreach (Producto a in list)
-        {
-            Console.WriteLine(a.ToString());
-        }
+        DatosTabla<Milanesas>.EliminarProducto(milanga);
 
         Console.ReadKey();
     }
