46faf06aa53b625d304fa0ccbb8ec2f50e5a8616
Fernando <eze980523@gmail.com>
2023-12-01 23:45:55 -0300

    Testeo de la clase Milanesas para DatosTabla<Milanesas>.AgregarObjeto().

--- pruebaSQLa/Program.cs
+++ pruebaSQLa/Program.cs
@@ -10,10 +10,11 @@ internal class Program
         Arroz arroz1 = new Arroz("Arroz blanco", "Arroz", (EMarca)Enum.Parse(typeof(EMarca), "Gallo"), 3, 500f, "Argentina", "Pablo");
         Gaseosa gaseosa1 = new Gaseosa("Seven up", "Gaseosa", (EMarca)Enum.Parse(typeof(EMarca), "SevenUp"), 3, 500f, 3f, "POMELO");
         GaseosaPorMayor gaseosaPorMayor1 = new GaseosaPorMayor("Trini", "Gaseosa Mayorista", (EMarca)Enum.Parse(typeof(EMarca), "Vienissima"), 4, 300f, 500f, "Uva", 1000, "Si");
+        Milanesas milanga = new Milanesas("Fernando", "Milanesas", (EMarca)Enum.Parse(typeof(EMarca), "Gallo"), 20, 300f, "Vacuno", "Brasil");
 
 
 
-        DatosTabla<GaseosaPorMayor>.AgregarObjeto(gaseosaPorMayor1);
+        DatosTabla<Milanesas>.AgregarObjeto(milanga);
 
         Console.ReadKey();
     }
