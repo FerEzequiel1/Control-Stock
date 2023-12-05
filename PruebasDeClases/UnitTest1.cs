23ca4fa7cc9bb37f8cfa3b5c815b7af6c98a6bd8
Fernando <eze980523@gmail.com>
2023-12-05 16:39:29 -0300

    Se agrega proyecto de pruebas unitarias

---
+++ PruebasDeClases/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PruebasDeClases
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
