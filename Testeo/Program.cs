8adac5cc030b93959be56fda8c03851dfebc5c87
Fernando <eze980523@gmail.com>
2023-12-05 15:26:21 -0300

    Testeo de la aplicaicon de hilos

--- Testeo/Program.cs
+++ Testeo/Program.cs
@@ -10,7 +10,26 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-       
+        Console.WriteLine($"Main hilo principal ={Thread.CurrentThread.ManagedThreadId}");
+
+
+        Task cargarProductos = new Task(() =>
+        {
+            Console.WriteLine($"Hilo Segundario ={Thread.CurrentThread.ManagedThreadId}");
+
+            for (int i = 1; i<5 ; i++)
+            {
+                Console.WriteLine($"Se ingreso producto {i}");
+            }
+        });
+
+        cargarProductos.Start();
+        Console.WriteLine("Empieza la carga de productos");
+
+        cargarProductos.Wait();
+
+        Console.WriteLine("Fin del hilo principal");
+
     }
 
 
